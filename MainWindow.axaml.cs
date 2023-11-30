14517c1cf0b2748d4d063f65ce2624d263638adc
eSuardOdin <esuardodin@gmail.com>
2023-11-30 10:33:54 +0100

    Initialisation du projet

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SchoolProjectA_ClientMVC;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
